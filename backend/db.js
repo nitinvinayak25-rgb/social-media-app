d2ca616e56ff56adabe36cf7096a969a4bed703d
nitinvinayak <nitinvinayak25@example.com>
2026-07-28 00:28:38 +0530

    Use Render database

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,9 @@
 const { Pool } = require("pg");
-
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "13042009",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
