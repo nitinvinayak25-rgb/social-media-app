c30a89c89d204dac82e429bcd573feac52c6f7d9
nitinvinayak <nitinvinayak25@example.com>
2026-07-28 16:40:27 +0530

    Fix process

--- backend/server.js
+++ backend/server.js
@@ -82,8 +82,8 @@ app.delete("/posts/:id", async (req, res) => {
   }
 });
 // Start server
-const PORT = Process.env.PORT || 5000 ;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
