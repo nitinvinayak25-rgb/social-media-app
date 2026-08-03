0d4047cec5cd2a1f59727545fb299d5a7d74aa18
nitinvinayak25-rgb <nitinvinayak25@gmail.com>
2026-08-03 14:08:44 +0530

    datacontext

--- frontend/src/context/DataContext.js
+++ frontend/src/context/DataContext.js
@@ -18,7 +18,7 @@ export const DataProvider = ({children}) => {
     const [editBody, setEditBody] = useState("");
     const navigate = useNavigate();
     const {width}=useWindowSize();
-    const {data,fetchError,isLoading} = useAxiosFetch("https://social-media-app-nofz.onrender.com")
+    const {data,fetchError,isLoading} = useAxiosFetch("https://social-media-app-nofz.onrender.com/posts");
 
     useEffect( () => {
     setPosts(data || []);
